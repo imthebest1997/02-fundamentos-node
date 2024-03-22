8d6aa6553c53ad9734e0b662732542a9193756c9
Alejandro G. <imthebest_1997@hotmail.com>
2024-03-21 23:17:48 -0500

    Migration

--- src/js-foundation/06-promises.ts
+++ src/js-foundation/06-promises.ts
@@ -1,6 +1,6 @@
 import { http } from "../plugins";
 
-export const getPokemonById = async( id: number ) => {
+export const getPokemonById = async( id: string | number ): Promise<string> => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
     const pokemon = await http.get(url);
